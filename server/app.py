a43833ba08c109ad3d64833590fcc3dba107a28a
Katelyn Luther <katelynluther87@aim.com>
2023-04-20 12:56:03 -0600

    deliverables met

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
 
 from models import db, Sweet, Vendor, VendorSweet
@@ -18,6 +18,55 @@ db.init_app(app)
 def home():
     return ''
 
+@app.route('/vendors')
+def vendors():
+    return make_response([vendor.to_dict() for vendor in Vendor.query.all()], 200)
+
+@app.route('/vendors/<int:id>')
+def vendors_by_id(id):
+    vendor = Vendor.query.filter_by(id=id).first()
+
+    if not vendor:
+        return make_response({'error': 'Vendor not found'}, 404)
+    return make_response(vendor.to_dict(), 200)
+
+@app.route('/sweets')
+def sweets():
+    return make_response([sweet.to_dict() for sweet in Sweet.query.all()], 200)
+
+@app.route('/sweets/<int:id>')
+def sweets_by_id(id):
+    sweet = Sweet.query.filter_by(id=id).first()
+
+    if not sweet:
+        return make_response({'error': 'Sweet not found'}, 404)
+    return make_response(sweet.to_dict(), 200)
+
+@app.route('/vendor_sweets', methods = ['POST'])
+def vendor_sweets():
+    new_vendor_sweet = VendorSweet(
+        price = request.get_json()['price'],
+        vendor_id = request.get_json()['vendor_id'],
+        sweet_id = request.get_json()['sweet_id']
+    )
+    db.session.add(new_vendor_sweet)
+    db.session.commit()
+
+    try:
+        return make_response(new_vendor_sweet.to_dict(rules=('sweet', 'vendor')), 201)
+    except ValueError:
+        return make_response({'error': 'Validation errors'}, 400)
+
+@app.route('/vendor_sweets/<int:id>', methods = ['DELETE'])
+def vendor_sweets_by_id(id):
+    vendor_sweet = VendorSweet.query.filter_by(id=id).first()
+    if not vendor_sweet:
+        return make_response({'error': 'VendorSweet not found'}, 404)
+    elif request.method == 'DELETE':
+        db.session.delete(vendor_sweet)
+        db.session.commit()
+
+        return make_response({}, 204)
     
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,3 +11,57 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+class Sweet(db.Model, SerializerMixin):
+    __tablename__ = 'sweets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    def __repr__(self):
+        return f'Name: {self.name}'
+    
+    vendor_sweets = db.relationship('VendorSweet', backref='sweet')
+    vendors = association_proxy('vendor_sweets', 'vendor')
+
+    serialize_rules = ('-created_at', '-updated_at', '-vendor_sweets', '-vendors')
+
+class VendorSweet(db.Model, SerializerMixin):
+    __tablename__ = 'vendor_sweets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'))
+    sweet_id = db.Column(db.Integer, db.ForeignKey('sweets.id'))
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 0:
+            raise ValueError('Validation errors')
+        return price
+
+    def __repr__(self):
+        return f'Price: {self.price} | vendor_id: {self.vendor_id}| sweet_id: {self.sweet_id}'
+
+    serialize_rules = ('-created_at', '-updated_at', '-vendor_id', '-sweet_id')
+
+class Vendor(db.Model, SerializerMixin):
+    __tablename__ = 'vendors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    vendor_sweets = db.relationship('VendorSweet', backref='vendor')
+    sweets = association_proxy('vendor_sweets', 'sweet')
+
+    def __repr__(self):
+        return f'Name: {self.name}'
+    
+    serialize_rules = ('-created_at', '-updated_at', '-vendor_sweets', '-sweets')
